0a5e7310d4c5c6c0b7b32573e7a5fe873f02fe56
dashu <zrui312715@gmail.com>
2021-06-03 11:26:26 +0800

    first commit

--- README.md
+++ README.md
@@ -1,2 +1,9 @@
+<<<<<<< HEAD
 # my-security-learning
 个人的Spring Security学习项目
+=======
+# 工程简介
+
+# 延伸阅读
+
+>>>>>>> 1954e44... first commit
---
+++ src/test/java/org/rhett/mysecurity/MySecurityApplicationTests.java
@@ -0,0 +1,13 @@
+package org.rhett.mysecurity;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MySecurityApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
